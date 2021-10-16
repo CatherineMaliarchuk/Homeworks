9e8a34b9556f479dc5db2b25796ab9178a45d05b
CatherineMaliarchuk <77687720+CatherineMaliarchuk@users.noreply.github.com>
2021-10-16 09:25:48 +0300

    Add files via upload

---
+++ HW_6.py
@@ -0,0 +1,6 @@
+mysql> CREATE TABLE Phones
+    -> (ID INT(11) NOT NULL AUTO_INCREMENT,
+    -> company_id INT(11) NOT NULL,
+    -> phone_name VARCHAR(30) NOT NULL,
+    -> CONSTRAINT contacts_pk PRIMARY KEY (Id));
+Query OK, 0 rows affected (0,08 sec)
